0f974ddb495290788af67257c2c1a833dcc37046
Linh Le <linh@le.id.au>
2012-06-18 01:56:21 +0800

    Fix to TEDxHovers js to not capture user profiles

--- sites/all/themes/tedx/js/tedx.hovers.js
+++ sites/all/themes/tedx/js/tedx.hovers.js
@@ -1,7 +1,7 @@
 jQuery.noConflict();
 jQuery(window).load(function(){
   jQuery('.views-row').click(function(){
-    if (jQuery(this).find('a').attr('href')){
+    if (!this.classList.contains("user-profile-listing-row") && jQuery(this).find('a').attr('href')){
       var href = jQuery(this).find('a').attr('href');
       window.location.href = href;
       return false;
