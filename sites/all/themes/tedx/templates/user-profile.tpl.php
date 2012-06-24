e07868f5e742a56d99bdffab518a6ff88e724f70
Linh Le <linh@le.id.au>
2012-06-24 22:33:29 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -1,4 +1,4 @@
-<h1><?php print $user_profile['field_first_name']['safe_value'] . ' ' . $user_profile['field_last_name']['safe_value']; ?></h1>
+<h1><?php print $user_profile['field_first_name'][0]['safe_value'] . ' ' . $user_profile['field_last_name'][0]['safe_value']; ?></h1>
 <?php print $user_profile['user_picture']['#markup']; ?>
 <h3>About Me</h3>
 <p><?php print $user_profile['field_about_me']['safe_value']; ?></p>
@@ -7,3 +7,7 @@
 <p><?php print $user_profile['field_about_me']['safe_value']; ?></p>
 <hr>
 <?php print_r($user_profile); ?>
+
+<?php if (isset($user_profile['field_facebook'][0]['safe_value'])): ?>
+    <?php print $user_profile['field_facebook'][0]['safe_value']; ?>
+<?php endif; ?>
