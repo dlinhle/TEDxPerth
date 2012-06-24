4fa8da53163ae36d1d67c9964ef8b8f278a8ac5f
Linh Le <linh@le.id.au>
2012-06-24 23:46:38 +0800

    Added recorded on date to Video node

--- sites/all/themes/tedx/templates/node--video.tpl.php
+++ sites/all/themes/tedx/templates/node--video.tpl.php
@@ -7,19 +7,22 @@
 ?>
 <h3>Videos</h3>
 <div class="grid_8 alpha">
-  <h1 class="single-title"><?php print $title; ?></h1>
+    <h1 class="single-title"><?php print $title; ?></h1>
+</div>
+<div class="grid_6 omega view-page-info">
+    Recorded on <?php print $content['field_reccorddate']['#items']['0']['value']; ?>
 </div>
 
 <div class="grid_10 alpha omega">
-  <?php if (!empty($content['field_vimeocode'])): ?>
+    <?php if (!empty($content['field_vimeocode'])): ?>
     <iframe src="http://player.vimeo.com/video/<?php print $content['field_vimeocode']['#items']['0']['value']; ?>?title=0&amp;byline=0&amp;portrait=0&amp;color=ff2b06" width="580" height="326" frameborder="0" webkitAllowFullScreen allowFullScreen></iframe>
-  <?php endif;?>
+    <?php endif;?>
 
-  <?php if (!empty($content['field_youtubecode'])):?>
+    <?php if (!empty($content['field_youtubecode'])):?>
     <iframe width="580" height="295" src="http://www.youtube.com/embed/<?php print $content['field_youtubecode']['#items']['0']['value'];?>" frameborder="0" allowfullscreen></iframe>
-  <?php endif;?>
+    <?php endif;?>
 
-  <?php
+    <?php
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
@@ -27,5 +30,5 @@
     hide($content['field_vimeocode']);
     hide($content['field_youtubecode']);
     print render($content);
-  ?>
+    ?>
 </div>
