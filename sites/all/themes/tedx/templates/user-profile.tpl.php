56eb484c0d6fa60c69254a7c57d8af6247beff3b
Linh Le <linh@le.id.au>
2012-06-24 22:39:37 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -4,10 +4,36 @@
 <p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
 
 <h3>Talk to Me About</h3>
-<p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
+<p><?php print $user_profile['field_talk_to_me_about'][0]['#markup']; ?></p>
 <hr>
 <?php print_r($user_profile); ?>
 
-<?php if (isset($user_profile['field_facebook']['und'][0]['safe_value'])): ?>
-    <?php print $user_profile['field_facebook']['und'][0]['safe_value']; ?>
+<?php if (isset($user_profile['field_facebook'][0]['#markup'])): ?>
+<div>
+    <?php print $user_profile['field_facebook'][0]['#markup']; ?>
+</div>
+<?php endif; ?>
+
+<?php if (isset($user_profile['field_twitter'][0]['#markup'])): ?>
+<div>
+    <?php print $user_profile['field_twitter'][0]['#markup']; ?>
+</div>
+<?php endif; ?>
+
+<?php if (isset($user_profile['field_googleplus'][0]['#markup'])): ?>
+<div>
+    <?php print $user_profile['field_googleplus'][0]['#markup']; ?>
+</div>
+<?php endif; ?>
+
+<?php if (isset($user_profile['field_linkedin'][0]['#markup'])): ?>
+<div>
+    <?php print $user_profile['field_linkedin'][0]['#markup']; ?>
+</div>
+<?php endif; ?>
+
+<?php if (isset($user_profile['field_youtubecode'][0]['#markup'])): ?>
+<div>
+    <?php print $user_profile['field_youtubecode'][0]['#markup']; ?>
+</div>
 <?php endif; ?>
