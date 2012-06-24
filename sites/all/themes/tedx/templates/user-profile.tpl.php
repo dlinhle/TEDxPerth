1132f364acbf9788d3f0f564fc3a03579f2e0585
Linh Le <linh@le.id.au>
2012-06-24 22:49:02 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -1,27 +1,40 @@
 <h1><?php print $user_profile['field_first_name'][0]['#markup'] . ' ' . $user_profile['field_last_name'][0]['#markup']; ?></h1>
-<?php print $user_profile['user_picture']['#markup']; ?>
-<h3>About Me</h3>
-<p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
+<div style="width:350px;display:inline">
+    <?php print $user_profile['user_picture']['#markup']; ?>
+</div>
+<div style="display:inline">
+    <h3>About Me</h3>
+    <p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
 
-<h3>Talk to Me About</h3>
-<p><?php print $user_profile['field_talk_to_me_about'][0]['#markup']; ?></p>
-<hr>
-
+    <h3>Talk to Me About</h3>
+    <p><?php print $user_profile['field_talk_to_me_about'][0]['#markup']; ?></p>
+    <hr>
+</div>
 <?php if (isset($user_profile['field_facebook'][0]['#markup'])): ?>
 <div>
-    <a target="_blank" href="http://www.facebook.com/<?php print $user_profile['field_facebook'][0]['#markup']; ?>">
-        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/facebook.png">
-        &nbsp; Find me on Facebook
-    </a>
+    <div style="display:inline">
+        <a target="_blank" href="http://www.facebook.com/<?php print $user_profile['field_facebook'][0]['#markup']; ?>">
+            <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/facebook.png"></a>
+    </div>
+
+    <div style="display:inline">
+        <a target="_blank" href="http://www.facebook.com/<?php print $user_profile['field_facebook'][0]['#markup']; ?>">Find me on Facebook
+        </a>
+    </div>
 </div>
 <?php endif; ?>
 
 <?php if (isset($user_profile['field_googleplus'][0]['#markup'])): ?>
 <div>
-    <a target="_blank" href="https://plus.google.com/<?php print $user_profile['field_googleplus'][0]['#markup']; ?>">
-        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/googleplus.png">
-        &nbsp; Find me on Google+
-    </a>
+    <div style="display:inline">
+        <a target="_blank" href="https://plus.google.com/<?php print $user_profile['field_googleplus'][0]['#markup']; ?>">
+            <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/googleplus.png"></a>
+    </div>
+    <div style="display:inline">
+        <a target="_blank" href="https://plus.google.com/<?php print $user_profile['field_googleplus'][0]['#markup']; ?>">
+            Find me on Google+
+        </a>
+    </div>
 </div>
 <?php endif; ?>
 
