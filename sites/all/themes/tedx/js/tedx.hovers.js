e27c6fbbf686b9a77f9526ad2e6966e92fe93719
Linh Le <linh@le.id.au>
2012-06-25 02:39:16 +0800

    Changes to js

--- sites/all/themes/tedx/js/tedx.hovers.js
+++ sites/all/themes/tedx/js/tedx.hovers.js
@@ -1,7 +1,8 @@
 jQuery.noConflict();
 jQuery(window).load(function(){
   jQuery('.views-row').click(function(){
-    if (!this.classList.contains("user-profile-listing-row") && jQuery(this).find('a').attr('href')){
+    if ((!this.classList.contains("user-profile-listing-row") || !this.classList.contains("tedx-team-members"))
+        && jQuery(this).find('a').attr('href')){
       var href = jQuery(this).find('a').attr('href');
       window.location.href = href;
       return false;
