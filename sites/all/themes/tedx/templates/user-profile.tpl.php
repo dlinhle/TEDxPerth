c172d2d9261108d40c57b4b87f619b68224f896c
Linh Le <linh@le.id.au>
2012-06-24 22:24:38 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -1,1 +1,9 @@
+<h1><?php print $user_profile['field_first_name'] + ' ' + $user_profile['field_last_name']; ?></h1>
+<?php print $user_profile['user_picture']['#markup']; ?>
+<h3>About Me</h3>
+<p><?php print $user_profile['field_about_me']; ?></p>
+
+<h3>Talk to Me About</h3>
+<p><?php print $user_profile['field_about_me']; ?></p>
+<hr>
 <?php print_r($user_profile); ?>
