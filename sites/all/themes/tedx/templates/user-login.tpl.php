63243a16218964526ad10ac07d565b7c913bd356
Linh Le <linh@le.id.au>
2012-06-24 22:17:10 +0800

    Begin amendments to view User profile page

--- sites/all/themes/tedx/templates/user-login.tpl.php
+++ sites/all/themes/tedx/templates/user-login.tpl.php
@@ -2,4 +2,4 @@
 <div class="yourtheme-user-login-form-wrapper">
     <?php print drupal_render_children($form) ?>
 </div>
-<p>Forgot your password? Click <a href="/user/password">here to reset</a>.</p>
+<p>Forgot your password? Click <a href="/user/password">here to reset your password</a>.</p>
