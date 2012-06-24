0fed50e2f37855010ea2cb773700df55acd3940b
Linh Le <linh@le.id.au>
2012-06-24 22:37:12 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -1,13 +1,13 @@
-<h1><?php print $user_profile['field_first_name'][0]['safe_value'] . ' ' . $user_profile['field_last_name'][0]['safe_value']; ?></h1>
+<h1><?php print $user_profile['field_first_name'][0]['#markup'] . ' ' . $user_profile['field_last_name'][0]['#markup']; ?></h1>
 <?php print $user_profile['user_picture']['#markup']; ?>
 <h3>About Me</h3>
-<p><?php print $user_profile['field_about_me']['safe_value']; ?></p>
+<p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
 
 <h3>Talk to Me About</h3>
-<p><?php print $user_profile['field_about_me']['safe_value']; ?></p>
+<p><?php print $user_profile['field_about_me'][0]['#markup']; ?></p>
 <hr>
 <?php print_r($user_profile); ?>
 
-<?php if (isset($user_profile['field_facebook'][0]['safe_value'])): ?>
-    <?php print $user_profile['field_facebook'][0]['safe_value']; ?>
+<?php if (isset($user_profile['field_facebook']['und'][0]['safe_value'])): ?>
+    <?php print $user_profile['field_facebook']['und'][0]['safe_value']; ?>
 <?php endif; ?>
