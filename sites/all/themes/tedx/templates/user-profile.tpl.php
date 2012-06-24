400c2c5ac342050bf7ca69a8f7805389fe5176de
Linh Le <linh@le.id.au>
2012-06-24 22:44:41 +0800

    Further amendments to view User profile page

--- sites/all/themes/tedx/templates/user-profile.tpl.php
+++ sites/all/themes/tedx/templates/user-profile.tpl.php
@@ -6,34 +6,48 @@
 <h3>Talk to Me About</h3>
 <p><?php print $user_profile['field_talk_to_me_about'][0]['#markup']; ?></p>
 <hr>
-<?php print_r($user_profile); ?>
 
 <?php if (isset($user_profile['field_facebook'][0]['#markup'])): ?>
 <div>
-    <?php print $user_profile['field_facebook'][0]['#markup']; ?>
-</div>
-<?php endif; ?>
-
-<?php if (isset($user_profile['field_twitter'][0]['#markup'])): ?>
-<div>
-    <?php print $user_profile['field_twitter'][0]['#markup']; ?>
+    <a target="_blank" href="http://www.facebook.com/<?php print $user_profile['field_facebook'][0]['#markup']; ?>">
+        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/facebook.png">
+        &nbsp; Find me on Facebook
+    </a>
 </div>
 <?php endif; ?>
 
 <?php if (isset($user_profile['field_googleplus'][0]['#markup'])): ?>
 <div>
-    <?php print $user_profile['field_googleplus'][0]['#markup']; ?>
+    <a target="_blank" href="https://plus.google.com/<?php print $user_profile['field_googleplus'][0]['#markup']; ?>">
+        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/googleplus.png">
+        &nbsp; Find me on Google+
+    </a>
 </div>
 <?php endif; ?>
 
 <?php if (isset($user_profile['field_linkedin'][0]['#markup'])): ?>
 <div>
-    <?php print $user_profile['field_linkedin'][0]['#markup']; ?>
+    <a target="_blank" href="http://www.linkedin.com/in/<?php print $user_profile['field_linkedin'][0]['#markup']; ?>">
+        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/linkedin.png">
+        &nbsp; Find me on LinkedIn
+    </a>
+</div>
+<?php endif; ?>
+
+<?php if (isset($user_profile['field_twitter'][0]['#markup'])): ?>
+<div>
+    <a target="_blank" href="http://www.twitter.com/<?php print $user_profile['field_twitter'][0]['#markup']; ?>">
+        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/twitter.png">
+        &nbsp; Find me on Twitter
+    </a>
 </div>
 <?php endif; ?>
 
 <?php if (isset($user_profile['field_youtubecode'][0]['#markup'])): ?>
 <div>
-    <?php print $user_profile['field_youtubecode'][0]['#markup']; ?>
+    <a target="_blank" href="http://www.youtube.com/user/<?php print $user_profile['field_youtubecode'][0]['#markup']; ?>">
+        <img src="/sites/all/modules/socialmedia/icons/levelten/glossy/32x32/youtube.png">
+        &nbsp; Find me on YouTube
+    </a>
 </div>
 <?php endif; ?>
