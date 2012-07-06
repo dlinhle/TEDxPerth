20ccca6e82004e7c41f8a7777b842ddd1b8ea2f6
Linh Le <linh@le.id.au>
2012-07-06 08:39:41 +0800

    Fix hover

--- sites/all/themes/tedx/js/tedx.hovers.js
+++ sites/all/themes/tedx/js/tedx.hovers.js
@@ -1,7 +1,7 @@
 jQuery.noConflict();
 jQuery(window).load(function(){
   jQuery('.views-row').click(function(){
-    if ((!this.classList.contains("user-profile-listing-row") || !this.classList.contains("tedx-team-members"))
+    if ((!this.classList.contains("user-profile-listing-row") || !this.classList.contains("tedx-team-members") || !this.classList.contains("clearfix"))
         && jQuery(this).find('a').attr('href')){
       var href = jQuery(this).find('a').attr('href');
       window.location.href = href;
